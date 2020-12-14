966c283d8eff1359508669c1571b889766c9c1a6
Bojarzin <47534935+Bojarzin@users.noreply.github.com>
2020-12-14 15:44:17 -0500

    trying to add chat

---
+++ Assets/_Scripts/ChatManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChatManager : MonoBehaviour
+{
+    public TMP_Text[] chatMessages;
+    public TMP_InputField chatInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateMessage()
+    {
+        Debug.Log("Hello");
+        for (int i = 0; i < chatMessages.Length; i++)
+        {
+            Debug.Log(chatMessages[1].text);
+            //chatMessages[i + 1].text = chatMessages[i].text;
+        }
+        chatMessages[0].text = chatInput.text;
+
+        chatInput.text = "";
+    }
+}
